39c68ba9e5c4b6eef04aa76d90993fcf3278c949
Ashley Montine Sproul <montinesproul@gmail.com>
2022-11-06 21:52:04 -0500

    first commit added html, css and js files started writing html and css code

---
+++ Develop/main.js
@@ -0,0 +1,7 @@
+//have a box in the center of the page
+// back and next button
+// mupltiple choice and t/f questions
+
+// what are your initials 
+    //save value to hight scores
+//submit button
